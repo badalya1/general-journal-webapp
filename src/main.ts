4ee7806092698548df9e2c77b28918ddd22e9cf6
Daniel Garcia <daniel.garcia@mail.utoronto.ca>
2020-07-07 06:51:43 -0400

    Use Vue Composition API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import "./env-setup";
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
+import "./register-service-worker";
 import router from "./router";
 import store from "./store";
 
@@ -9,5 +10,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
